ebcfbdf0507a4c58cfc3ce77044d181458912eca
leonardodppaim <leonardodppaim@gmail.com>
2023-04-20 22:30:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function adicionar() {
-    var numero = document.getElementById('entrada1').innerHTML;
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const adicionadar = async () =>{
+try {  
+    const numero = core.getInput('numero1', { required: true });
+    
     if(numero)
     {
         document.getElementById('resultado').innerHTML = numero;
     }
     else
     {
-        document.getElementById('resultado').innerHTML = "Nada..."
+        document.getElementById('resultado').innerHTML = "Pegadinha do malandro";
     }
     console.log("Concluido");
+}
+}
+cath (error){
+    core.setFailed(error.message);
 }
 
 
